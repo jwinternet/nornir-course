c5b35603c5258eb70182a83bc181eaefd9e1bf55
jwinternet <47834941+Killface0@users.noreply.github.com>
2020-11-21 10:12:13 -0500

    Lesson 2, Exercise 5

---
+++ Lesson 2/L5E5A.py
@@ -0,0 +1,5 @@
+from nornir import InitNornir
+from nornir.core.filter import F
+from nornir.plugins.functions.text import print_result
+from nornir.plugins.tasks.networking import netmiko_send_command
+
