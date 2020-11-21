141c3f52c5db5666077c5865b72c0a557d45f642
jwinternet <47834941+Killface0@users.noreply.github.com>
2020-11-21 07:56:05 -0500

    Lesson 2 Exercise 2A

---
+++ Lesson 2/L2E2A.py
@@ -0,0 +1,15 @@
+from nornir import InitNornir
+from nornir.core.filter import F
+
+
+def main():
+	nr = InitNornir(config_file="config.yaml")
+	filt = F(groups__contains="ios")
+	nr = nr.filter(filt)
+	print()
+	print(nr.inventory.hosts)
+	print()
+
+
+if __name__ == "__main__":
+	main()
