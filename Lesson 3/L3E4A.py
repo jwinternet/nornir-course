15ef594f5e2e5e0d6244b4a1128010692e080180
jwinternet <47834941+Killface0@users.noreply.github.com>
2020-11-22 04:43:45 -0500

    Lesson 3, Exercise 4B

--- Lesson 3/L3E4A.py
+++ Lesson 3/L3E4A.py
@@ -1,8 +1,21 @@
+# from pprint import pprint
 from nornir import InitNornir
+from nornir.core.filter import F
+from nornir.plugins.tasks.networking import netmiko_send_command
 
 
 def main():
 	nr = InitNornir(config_file="config.yaml")
+	nr = nr.filter(F(groups__contains="eos"))
+	agg_result = nr.run(
+			task=netmiko_send_command,
+			command_string="show int status",
+			use_textfsm=True)
+	print()
+	print("Exercise 4b - verify structured data")
+	print("-" * 20)
+	print(type(agg_result["arista1"][0].result))
+	print("-" * 20)
 
 
 if __name__ == "__main__":
