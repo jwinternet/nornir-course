3ec7f1fd842a78ddab03db1a5dc58aab538b6479
jwinternet <47834941+Killface0@users.noreply.github.com>
2020-11-22 04:51:14 -0500

    Lesson 3, Exercise 4B

--- Lesson 3/L3E4A.py
+++ Lesson 3/L3E4A.py
@@ -1,4 +1,4 @@
-# from pprint import pprint
+from pprint import pprint
 from nornir import InitNornir
 from nornir.core.filter import F
 from nornir.plugins.tasks.networking import netmiko_send_command
@@ -16,6 +16,23 @@ def main():
 	print("-" * 20)
 	print(type(agg_result["arista1"][0].result))
 	print("-" * 20)
+
+	print("\nExercise 4c - final dictionary")
+	print("-" * 20)
+	combined_data = {}
+	for device_name, multi_result in agg_result.items():
+		combined_data[device_name] = {}
+		device_result = multi_result[0]
+		for intf_dict in device_result.result:
+			intf_name = intf_dict["port"]
+			inner_dict = {
+				"status": intf_dict["status"],
+				"vlan": intf_dict["vlan"]
+			}
+			combined_data[device_name][intf_name] = inner_dict
+	pprint(combined_data)
+	print("-" * 20)
+	print()
 
 
 if __name__ == "__main__":
