5b9c86afa011abbfdc292fb921cf2ed8b0f7c8c1
jwinternet <47834941+Killface0@users.noreply.github.com>
2020-11-29 11:05:46 -0500

    Lesson 2, Exercise 2C, version 4

---
+++ Lesson 2/L2E2C.py
@@ -0,0 +1,21 @@
+from nornir import InitNornir
+from nornir.core.filter import F
+from nornir.plugins.tasks.networking import netmiko_send_command
+
+
+def main():
+	nr = InitNornir(config_file="config.yaml")
+	filt = F(groups__contains="ios")
+	nr = nr.filter(filt)
+	my_results = nr.run(
+			task=netmiko_send_command, command_string="show ver | i uptime"
+	)
+	host_results = my_results["cisco3"]
+	print(
+			type(host_results),
+			repr(host_results[0]),
+			host_results.__iter__
+	)
+
+if __name__ == "__main__":
+	main()
