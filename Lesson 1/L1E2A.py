1d41453b74f9b47f223e33c4b67d254ca3aade74
jwinternet <47834941+Killface0@users.noreply.github.com>
2020-11-29 09:49:32 -0500

    Lesson 1, Exercise 2A, version 4 - Further adjusted formatting.

--- Lesson 1/L1E2A.py
+++ Lesson 1/L1E2A.py
@@ -6,14 +6,14 @@ def main():
 	nr = InitNornir()
 	for host_name, host_obj in nr.inventory.hosts.items():
 		print(
-				f"\nHost: {host_name}",
+				f"\nHost: {host_name.title()}",
 				"-" * 20,
 				f"\n\nHostname: {host_obj.hostname}",
 				f"\nGroups: {host_obj.groups}",
 				f"\nPlatform: {host_obj.platform}",
 				f"\nUsername: {host_obj.username}",
 				f"\nPassword: {host_obj.password}",
-				f"\nPort: {host_obj.port}",
+				f"\nPort: {host_obj.port}\n",
 		)
 
 
