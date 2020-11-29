82ce7a913593d6422e79e32f420d4ff747e2ea93
jwinternet <47834941+Killface0@users.noreply.github.com>
2020-11-29 09:47:14 -0500

    Lesson 1, Exercise 2A, version 4 - Created several new files.

---
+++ Lesson 1/L1E2A.py
@@ -0,0 +1,21 @@
+from nornir import InitNornir
+
+
+def main():
+	""""""
+	nr = InitNornir()
+	for host_name, host_obj in nr.inventory.items():
+		print(
+				f"Host: {host_name}",
+				"-" * 20,
+				f"Hostname: {host_obj.hostname}",
+				f"Groups: {host_obj.groups}",
+				f"Platform: {host_obj.platform}",
+				f"Username: {host_obj.username}",
+				f"Password: {host_obj.password}",
+				f"Port: {host_obj.port}",
+		)
+
+
+if __name__ == "__main__":
+	main()
